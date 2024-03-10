b805bd2044214050763b3db30614049bcb1683f2
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-03-10 18:23:07 -0400

    heade html complete

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, [FontAwesomeModule]],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Client';
+  faCoffee = faCoffee;
 }
--- Client/src/app/header/header.component.ts
+++ Client/src/app/header/header.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+import {faBell, faGear, faQuestion, faSearch, faTimes, faUser} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    FaIconComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  appLauncherIcon = 'assets/images/app-launcher-icon.png'
+  CeasarColouredLogo1 = "assets/images/ceasar-coloured-logo-1.svg";
+  faSearch = faSearch;
+  protected readonly faTimes = faTimes;
+  protected readonly faQuestion = faQuestion;
+  protected readonly faGear = faGear;
+  protected readonly faBell = faBell;
+  protected readonly faUser = faUser;
 }
