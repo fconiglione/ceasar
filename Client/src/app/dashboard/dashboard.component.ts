c36a8393dc39742a28a4477fffc41549ed8f7110
fconiglione <122475578+fconiglione@users.noreply.github.com>
2024-03-13 23:21:57 -0400

    Added responsive workspace dashboard

--- Client/src/app/dashboard/dashboard.component.ts
+++ Client/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons";
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faEllipsisVertical} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,4 +16,6 @@ export class DashboardComponent {
 
   protected readonly faArrowRightLong = faArrowRightLong;
   BlankWorkspace = "assets/images/blank-workspace-img.png";
+  CeasarColouredLogo2 = "assets/images/ceasar-coloured-logo-2.svg";
+  faEllipsisVertical = faEllipsisVertical;
 }
