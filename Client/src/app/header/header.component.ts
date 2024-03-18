c9d6ab76bae5b67ddffc6c87b5a329d1fb9c8ced
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-03-17 23:18:40 -0400

    started app launchere

--- Client/src/app/header/header.component.ts
+++ Client/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
 import {faBell, faGear, faQuestion, faSearch, faTimes, faUser, faArrowRight} from '@fortawesome/free-solid-svg-icons';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
@@ -14,8 +14,29 @@ import {FaIconComponent} from "@fortawesome/angular-fontawesome";
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(private elementRef: ElementRef) {}
+  isActive: boolean = false;
+  openAppLauncher() {
+    this.isActive = true;
+
+    const overlay = this.elementRef.nativeElement.querySelector('#overlay');
+    overlay.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  closeAppLauncher() {
+    this.isActive = false;
+
+    const overlay = this.elementRef.nativeElement.querySelector('#overlay');
+    overlay.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
   appLauncherIcon = 'assets/images/app-launcher-icon.png'
   CeasarColouredLogo1 = "assets/images/ceasar-coloured-logo-1.svg";
+  CeasarColouredLogo2 = "assets/images/ceasar-coloured-logo-2.svg";
   faSearch = faSearch;
   faArrowRight = faArrowRight;
   protected readonly faTimes = faTimes;
