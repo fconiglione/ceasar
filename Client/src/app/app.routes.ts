6157b0554f7c530eb437d027b20f6146802dfbb2
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-05-02 13:41:36 -0400

    updated link redirects

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { SearchComponent } from './pages/search/search.component';
+import { WorkspaceComponent } from './pages/workspace/workspace.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'ws', component: WorkspaceComponent}
 ];
