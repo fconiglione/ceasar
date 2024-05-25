ab6eeaf50b31fd8f4b2d25646f1df36a99d3e9ba
Francesco Coniglione <122475578+fconiglione@users.noreply.github.com>
2024-05-24 23:11:31 -0400

    started lead caard

--- Client/src/app/components/features/leads/leads.component.ts
+++ Client/src/app/components/features/leads/leads.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
-import { faChevronDown, faSearch, faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faChevronDown, faSearch, faDownload, faPhone, faEnvelope, faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-leads',
@@ -15,5 +15,9 @@ export class LeadsComponent {
   faChevronDown = faChevronDown;
   faSearch = faSearch;
   faDownload = faDownload;
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
+  faEllipsisV = faEllipsisV;
+  DefaultPFP = "assets/images/default-pfp.svg";  ;
   constructor() { }
 }
